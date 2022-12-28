e518e9277251a1cfea0288b74f9a7f62c7491ddc
Bartłomiej Konecki <bartekkonecki97@gmail.com>
2022-12-28 18:03:45 +0100

    Updated comments in main()

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * First divide inputs for parameters and values and store them in separate lists
+         */
         for(int i = 0; i < args.length; i++)
             args[i] = args[i].toUpperCase();
 
@@ -18,13 +21,19 @@ public class Main {
     }
 
     public static void processInputs(List<String> parameters, List<String> values){
+        /**
+         * Dispaly help if input is incorrect
+         * or if user asks for help
+         */
         if(parameters.isEmpty() || parameters.get(0).charAt(0) == 'H' || parameters.get(0).charAt(0) == '?')
             displayManual();
 
+        /**
+         * Set input system and ouput system
+         */
         int inputSystem;
         int outputSystem;
 
-        //Getting the input system
         if(parameters.get(0).charAt(0) == 'R')
             inputSystem = 0;
         else if(parameters.get(0).charAt(0) == 'B')
@@ -38,8 +47,6 @@ public class Main {
         else
             inputSystem = Integer.parseInt(parameters.get(0));
 
-
-        //Getting the output system
         if(parameters.get(1).charAt(0) == 'R')
             outputSystem = 0;
         else if(parameters.get(1).charAt(0) == 'B')
@@ -55,8 +62,15 @@ public class Main {
 
         System.out.println("parameters" + parameters);
         System.out.println("values" + values);
+        System.out.println(changeNumberSystem(Integer.parseInt(values.get(0)), inputSystem, outputSystem));
+
     }
 
+    /**
+     * Function translates number from roman system to decimal system
+     * @param roman number to be translated
+     * @return Intiger translated from roman number
+     */
     public static int romanToDecimal(String roman){
         //I V X L C D M
         roman = roman.toUpperCase();
@@ -78,14 +92,29 @@ public class Main {
         return -1;
     }
 
+    /**
+     * Function translates number from decimal systemto romrna system
+     * @param decimal number to be translated
+     * @return String roman number
+     */
     public static String decimalToRoman(int decimal){
         return "work in progress";
     }
 
-    public static String changeNumberSystem(int input, String inputSystem, String outputSystem){
+    /**
+     * Function translates number from inputSystem to outputSystem
+     * @param input number to be translated given in inputSystem
+     * @param inputSystem system of number to be translated from
+     * @param outputSystem system of number to be translated to
+     * @return ouputs result of the translation as String
+     */
+    public static String changeNumberSystem(int input, int inputSystem, int outputSystem){
         return "work in progress";
     }
 
+    /**
+     * Display manual for user and exit, same as README.md
+     */
     public static void displayManual(){
         System.out.println("""
                 # Number converter

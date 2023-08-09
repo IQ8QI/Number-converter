46a2d93f2c7a15a564c23d9d759658c7f9bbbb7d
Bartłomiej Konecki <bartekkonecki97@gmail.com>
2023-08-09 07:15:14 +0200

    Updated ConvertTest.java
    Added tests for romanToDecimal and decimalToRoman.
    Now checking also if functions properly throw exceptions.

--- src/test/java/ConvertTest.java
+++ src/test/java/ConvertTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.InputMismatchException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ConvertTest {
 
@@ -12,11 +16,18 @@ public class ConvertTest {
         assertEquals("XXIV", Convert.decimalToRoman(24), "24 should be XXIV");
         assertEquals("LXXXIV", Convert.decimalToRoman(84), "84 should be converted to LXXXIV");
         assertEquals("CDLXIX", Convert.decimalToRoman(469), "469 should be converted to CDLXIX");
+        assertThrows(InputMismatchException.class, () -> Convert.decimalToRoman(4000), "4000 is to big for program to convert");
+        assertThrows(InputMismatchException.class, () -> Convert.decimalToRoman(0), "0 is to small to be converted");
     }
     @Test
     @DisplayName("Testing Convert.romanToDecimal function")
     void testRomanToDecimal(){
         assertEquals(2485, Convert.romanToDecimal("MMCDLXXXV"), "MMCDLXXXV should be converted to 2485");
         assertEquals(3999, Convert.romanToDecimal("MMMCMXCIX"), "MMMCMXCIX should be converted to 3999");
+        assertEquals(1, Convert.romanToDecimal("I"), "I should be converted to 1");
+        assertEquals(249, Convert.romanToDecimal("CCXLIX"), "CCXLIX should be converted to 249");
+        assertThrows(InputMismatchException.class, () -> Convert.romanToDecimal("MMMM"), "Number MMMM is to big to be converted");
+        assertThrows(InputMismatchException.class, () -> Convert.romanToDecimal("CCH"), "H is not part of Roman system");
+        assertThrows(InputMismatchException.class, () -> Convert.romanToDecimal("XX1"), "1 is not part of Roman system");
     }
 }
